a15179e5862d244146d6426eca8c8ebd0a79df40
Jesse Roe <roeje@mail.gvsu.edu>
2017-01-23 23:58:00 -0500

    update simple shell with cpu time and context switch logging

--- Lab2/simple_shell.c
+++ Lab2/simple_shell.c
@@ -9,6 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <time.h>
 #include <sys/resource.h>
 
@@ -20,10 +23,11 @@ void background (char * cmd);
 
 int main() {
 
-   pid_t childPID;
-   int childStatus;
+   pid_t child_pid, pid;
+   int child_status, result;
    char cmd[MAXLINE];
-   struct rusage *stats;
+   struct rusage usage;
+   int who = RUSAGE_CHILDREN;
    // printf("mysh$ ");
    for (; ;) {
 
@@ -33,25 +37,35 @@ int main() {
       fgets(cmd, MAXLINE, stdin);
 
       // if the command in cmd is “exit\n”, then terminate this program;
-      if (strcmp(cmd,"exit\n") == 0) {
-         printf("Exit...\n");
+      if (strcmp(cmd,"quit\n") == 0) {
+         printf("Exiting...\n");
          exit(0);
       }
 
       // otherwise, create a child process to handle that command.
-      int value = fork();
-      if (value == 0) {
+      pid = fork();
+      if (pid) {
+
+         /*struct timeval {
+             time_t      tv_sec;
+             suseconds_t tv_usec;
+         }*/
+
+         // Wait for child process to terminate
+         child_pid = waitpid(-1, &child_status, 0);
+         result = getrusage(who, &usage);
+         printf("User CPU time used: %ld.%06ld\n", usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
+         printf("Number of involuntary context switches: %li\n", usage.ru_nivcsw);
+
+      } else if (pid < 0) {
+         printf("An error occured spawning a child process. Exiting.\n");
+         exit(-1);
+      } else {
          background(cmd);
+
          exit(0);
       }
-
-      // Wait for child process to terminate
-      childPID = wait(&childStatus);
-      printf("%s\n", childPID);
-      int result = getrusage(childPID, stats);
-      printf("CSWITCH TIME: %ld\n", stats->ru_nivcsw);
    }
-
 	return 0;
 }
 
@@ -62,7 +76,7 @@ void background (char *cmd) {
    char *argv[MAXARG];
 
    tmp = strtok(cmd, "\t \n");
-   while(tmp != NULL && tmp != "\0") {
+   while(tmp != NULL) {
       argv[i] = tmp;
       tmp = strtok(NULL, "\t \n");
       i = i + 1;
