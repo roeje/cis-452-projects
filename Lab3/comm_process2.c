d6fb918e8f816537be14abffe46b2545c4478c8f
roeje <jessedroe@gmail.com>
2017-02-09 14:07:14 -0500

    update to comm process

--- Lab3/comm_process2.c
+++ Lab3/comm_process2.c
@@ -10,31 +10,49 @@
 #include <signal.h>
 
 /**
- * CIS 452 - Lab 3: Communicating Process
+ * CIS 452 - Lab 3: EC Communicating Process
+ *
+ * Creates child process and handles signals sent by child
+ *
  * @author  Jesse Roe
  * @author  Michael Kolarik
  * @version 01/22/2016
  */
 
 
-void sigHandler(int sigNum, siginfo_t *siginfo, void *context);
+void sigHandler(int sigNum);
 void exitHandler(int sigNum);
 
 int main() {
 
-   struct sigaction sig1, sig2;
 
-   memset(&sig1, '\0', sizeof(sig1));
-   memset(&sig2, '\0', sizeof(sig2));
+  // struct sigaction new_action, old_action;
+  //
+  // new_action.sa_handler = termination_handler;
+  // sigemptyset (&new_action.sa_mask);
+  // new_action.sa_flags = 0;
 
    srand(time(NULL));
-   // signal (SIGUSR1, sigHandler);
-   sig1.sa_sigaction = &sigHandler;
-   sig1.sa_flags = SA_SIGINFO;
+   struct sigaction sig1, sig2;
 
-   sig2.sa_sigaction = &sigHandler;
-   sig2.sa_flags = SA_SIGINFO;
-   // signal (SIGUSR2, sigHandler);
+  //  memset(&sig1, '\0', sizeof(sig1));
+  //  memset(&sig2, '\0', sizeof(sig2));
+   //
+  //  sig1.sa_sigaction = &sigHandler;
+  //  sig1.sa_flags = SA_SIGINFO;
+   //
+  //  sig2.sa_sigaction = &sigHandler;
+  //  sig2.sa_flags = SA_SIGINFO;
+
+   sig1.sa_handler = sigHandler;
+   sig2.sa_handler = sigHandler;
+
+   sigemptyset (&sig1.sa_mask);
+   sigemptyset (&sig2.sa_mask);
+
+   sig1.sa_flags = 0;
+   sig2.sa_flags = 0;
+
 
    sigaction(SIGUSR1, &sig1, NULL);
    sigaction(SIGUSR2, &sig2, NULL);
@@ -51,12 +69,6 @@ int main() {
 
    // otherwise, create a child process to handle that command.
    parent_pid = getpid();
-
-   // pid = fork();
-
-  //  signal (SIGUSR1, sigHandler);
-  //  signal (SIGUSR2, sigHandler);
-  //  signal (SIGINT, exitHandler);
 
    int i;
    for (i = 0; i < 2; i++) {
@@ -75,46 +87,49 @@ int main() {
             int user_sig = rand()%2;
 
             if (user_sig) {
+               printf("Killing1...\n");
                kill(parent_pid, SIGUSR1);
 
-            }else {
+            } else {
+               printf("Killing2...\n");
                kill(parent_pid, SIGUSR2);
-            }
+            }          
 
          }
          return 0;
       }
    }
 
-
-   // if in parent process
-
-   signal (SIGINT, exitHandler);
-
    // Wait for child process to terminate
-   // printf("waiting...   ");
-   // fflush(stdout);
-   waitpid(-1, &status, 0);
+   //  waitpid(-1, &status, 0);
+   pause();
    return 0;
-
 }
 
-void sigHandler (int sigNum, siginfo_t *siginfo, void *context) {
-    // printf (" received an interrupt.\n");
-   if (sigNum == SIGUSR1) {
-      printf ("received a SIGUSR1 signal.\n");
+// void sigHandler (int sigNum, siginfo_t *siginfo, void *context) {
+//    printf("SigHandler...");
+//    if (sigNum == SIGUSR1) {
+//       printf ("received a SIGUSR1 signal from PID: %ld\n", (long)siginfo->si_pid);
+//    }
+//    if (sigNum == SIGUSR2) {
+//       printf ("received a SIGUSR2 signalfrom PID: %ld\n", (long)siginfo->si_pid);
+//    }
+// }
+//
+//
+// void exitHandler (int sigNum) {
+//     printf(" received.  That's it, I'm shutting you down...\n");
+//     exit(0);
+// }
 
-      // signal (SIGUSR1, sigHandler);
-      // signal (SIGINT, exitHandler);
+void sigHandler (int sigNum) {
+   if (sigNum == SIGUSR1) {
+      printf ("received a SIGUSR1 signal from PID:\n");
    }
    if (sigNum == SIGUSR2) {
-      printf ("received a SIGUSR2 signal.\n");
-
-      // signal (SIGUSR2, sigHandler);
-      // signal (SIGINT, exitHandler);
+      printf ("received a SIGUSR2 signalfrom PID: \n");
    }
 }
-
 
 void exitHandler (int sigNum) {
     printf(" received.  That's it, I'm shutting you down...\n");
