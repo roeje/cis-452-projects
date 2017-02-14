d5370d09c1ed4a6af6df1fcec26d6fab3d1280e1
Jesse Roe <roeje@mail.gvsu.edu>
2017-02-14 17:45:12 -0500

    more updates to program 2

--- Program2/stream_vector_proc.c
+++ Program2/stream_vector_proc.c
@@ -10,10 +10,9 @@
 #include <signal.h>
 
 /**
- * CIS 452 - Lab 3: Communicating Process
- * @author  Jesse Roe
- * @author  Michael Kolarik
- * @version 01/22/2016
+ * CIS 452 - Project 2
+ * @author  Jesse Roe *
+ * @version 02/10/2017
  */
  #define READ 0
  #define WRITE 1
@@ -71,50 +70,66 @@ int main() {
 
          if (i == 0) {
             // Spawn Increment Process
-            FILE *read, *write;
 
-            char pipe_buffer[binary_length];
+            char pipe_buffer[binary_length + 1];
 
-            read = fdopen(pipe_one[READ], "r");
-            write = fdopen(pipe_two[WRITE], "w");
+            close(pipe_one[WRITE]);
+            close(pipe_two[READ]);
+
+            // read = fdopen(pipe_one[READ], "r");
+            // write = fdopen(pipe_two[WRITE], "w");
 
             while(1) {
-               fread(pipe_buffer, binary_length, 1, read);
+               // fread(pipe_buffer, sizeof(pipe_buffer) + 1, 1, read);
+               read(pipe_one[READ], pipe_buffer, sizeof(pipe_buffer) + 1);
 
-               fprintf(stdout, "Incrementer: Read binary string: %s\n", pipe_buffer);
+               fprintf(stdout, "Incrementer: Read binary string from PIPE: %s\n", pipe_buffer);
                fflush(stdout);
 
                char *binary_output = pipe_buffer;
 
-               fwrite(pipe_buffer, binary_length, 1, write);
+               // fwrite(pipe_buffer, sizeof(pipe_buffer) + 1, 1, write);
+               write(pipe_two[WRITE], pipe_buffer, sizeof(pipe_buffer) + 1);
+               // fflush(pipe_two[WRITE]);
             }
 
 
          } else {
             // Spawn Adder Process
-            FILE *read, *input_file, *output_file;
+            FILE *input_file, *output_file;
 
-            char pipe_buffer[binary_length];
+            char pipe_buffer[binary_length + 1];
             char input_file_buffer[binary_length];
 
-            read = fdopen(pipe_two[READ], "r");
+            close(pipe_one[WRITE]);
+            close(pipe_one[READ]);
+            close(pipe_two[WRITE]);
+
+            // read = fdopen(pipe_two[READ], "r");
             input_file = fopen(fileB, "r");
             output_file = fopen("output.dat", "w");
 
             while(1) {
-               fread(pipe_buffer, binary_length, 1, read);
-               int success = fread(input_file_buffer, binary_length, 1, input_file);
+               // fread(pipe_buffer, sizeof(pipe_buffer) + 1, 1, read);
+               read(pipe_two[READ], pipe_buffer, sizeof(pipe_buffer) + 1);
+               // read(pipe_read ,pipe_buffer, binary_length);
 
-               if (success == 0) {
+               if (fgets(input_file_buffer, binary_length + 3, input_file) == NULL) {
                   break;
                }
+
+               // clean input of newlines and CR
+               input_file_buffer[strcspn(input_file_buffer, "\r\n")] = 0;
 
                fprintf(stdout, "Adder: Read binary string from Pipe: %s\n", pipe_buffer);
                fprintf(stdout, "Adder: Read binary string from fileB: %s\n", input_file_buffer);
                fflush(stdout);
 
                char *binary_output = pipe_buffer;
-               fwrite(pipe_buffer, binary_length, 1, output_file);
+               // fwrite(pipe_buffer, sizeof(pipe_buffer) + 1, 1, output_file);
+               fprintf(output_file, "%s\n", pipe_buffer);
+
+
             }
          }
 
@@ -122,38 +137,41 @@ int main() {
       }
    }
 
-
-
    // Parent Process - Complementer
 
-   FILE *input_file, *write;
+   FILE *input_file;
 
-   char file_buffer[binary_length];
+   char file_buffer[binary_length + 1];
+
+   close(pipe_one[READ]);
+   close(pipe_two[READ]);
+   close(pipe_two[WRITE]);
 
    input_file = fopen(fileA, "r");
-   write = fdopen(pipe_one[WRITE], "w");
+   // write = fdopen(pipe_one[WRITE], "w");
 
    fprintf(stdout, "Waiting for Ctrl^C confirmation...\n");
    while (initial_pause) {
       ;
    }
+   // signal (SIGINT, exit_handler);
 
-   while(1) {
-      int success = fread(file_buffer, binary_length, 1, input_file);
+   while(fgets(file_buffer, binary_length + 3, input_file) != NULL) {
 
-      if (success == 0) {
-         break;
-      }
+      // remove newline and any carrage returns (windows)
+      file_buffer[strcspn(file_buffer, "\r\n")] = 0;
 
       fprintf(stdout, "Complementer: Read binary string: %s\n", file_buffer);
       fflush(stdout);
 
       char *binary_output = file_buffer;
 
-      fwrite(file_buffer, binary_length, 1, write);
+      // fwrite(file_buffer, , 1, write);
+      write(pipe_one[WRITE], file_buffer, sizeof(file_buffer) + 1);
+      // fflush(pipe_one[WRITE]);
    }
 
-   // signal (SIGINT, exit_handler);
+   signal (SIGINT, exit_handler);
    waitpid(-1, &status, 0);
    return 0;
 }
@@ -163,7 +181,7 @@ int main() {
 void pause_handler (int sigNum) {
     // printf (" received an interrupt.\n");
    if (initial_pause == 1) {
-      fprintf (stdout, "Starting Processsing...\n");
+      fprintf (stdout, " Starting Processsing...\n");
       initial_pause = 0;
 
    }
