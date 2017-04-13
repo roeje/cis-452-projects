1319708f058b4a7a73bb6bf80eb4ba15ed215ab0
Jesse Roe <jessedroe@gmail.com>
2017-04-13 01:21:50 -0400

    add program for lab12

--- Lab12/program1.c
+++ Lab12/program1.c
@@ -5,35 +5,49 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <pwd.h>
+#include <grp.h>
 
 int main(int argc, char *argv[]) {
    DIR *dirPtr;
    struct dirent *entryPtr;
    struct stat statBuf;
    int i = 0;
+   int file_count = 0;
    char *pathname;
    char *commands;
+   char *command;
 
    if (argc == 0) {
      printf("Path name required\n");
      exit(1);
    } else {
-     pathname = argv[argc - 1];
-     commands = argv[argc - 2];
+      pathname = argv[argc - 1];
+      commands = argv[argc - 2];
+      command = &commands[1];
+      // printf("Commands: %s\n", commands);
+      // printf("Command %s\n", command);
+      // printf("Path: %s\n", pathname);
    }
-   dirPtr = opendir (*pathname);
-
-   if (commands)
+   dirPtr = opendir (pathname);
 
    while ((entryPtr = readdir (dirPtr))) {
+      char full_path[255];
+      sprintf(full_path, "%s/%s", pathname, entryPtr->d_name);
 
-      if (stat (entryPtr->d_name, &statBuf) < 0) {
+      if (stat (full_path, &statBuf) < 0) {
         perror ("huh?  there is ");
         exit(1);
       }
+      if (strcmp(command, "l") == 0) {
+         // char * file_type = (int)((statBuf.st_mode)&77000) + '0';
+         printf ("%-8.3o %-8d %-8d %-8s %-8s %-8s\n", (statBuf.st_mode)&0777, (int)statBuf.st_size, (int)statBuf.st_nlink, getpwuid(statBuf.st_uid)->pw_name, getgrgid(statBuf.st_gid)->gr_name, entryPtr->d_name);
+      }
+      if (strcmp(command, "i") == 0) {
+         printf ("%d %-20s\n",(int)statBuf.st_ino, entryPtr->d_name);
+      }
 
-      printf ("Filename: %-20s Size(bytes): %d\n", entryPtr->d_name, (int)statBuf.st_size);
-    }
+   }
    closedir (dirPtr);
    return 0;
 }
